790ab6c43988eb24e91eeeea682c99bd671d5502
Patrick <patrickzm290493@gmail.com>
2025-10-26 18:26:44 -0300

    ajustado config

--- tests/support/fixtures/users.ts
+++ tests/support/fixtures/users.ts
@@ -6,7 +6,7 @@ const getEnvOrThrow = (key: string): string => {
   const value = process.env[key];
   if (!value) {
     throw new Error(
-      `Variável de ambiente ${key} não configurada. Configure o arquivo .env`
+      `Por favor revise o arquivo .env`
     );
   }
   return value;
